615e0a7293bc742b48a43fd1be700efe0e59a224
Tyler Degen <degen.19@osu.edu>
2016-08-04 16:03:13 -0400

    more code tidying and refactoring

--- conversions/MAVPOL.R
+++ conversions/MAVPOL.R
@@ -101,7 +101,7 @@ for(i in 1:numberofsegments){
 		precedingsegment <- "NA"
 		followingsegment <- "NA"
 		
-        cat("i: ", i, "\n")
+        
 		if (i > 1){
 			precedingsegment <- praat("Get label of interval...", input=FullPath(textgridname), arguments=list(1,i-1))
 		}
@@ -116,33 +116,19 @@ for(i in 1:numberofsegments){
 			followingsegment <- "NA"
 		}
 		
-        #no good way to comment out code in R so going to use if(FALSE)
-        
-
         
 		#selected object is now formant name...might be worth just making that a variable?
         f1 <- praat("Get value at time...", input=FullPath(formantname), arguments=list(1, mid, "Hertz", "Linear"))
         f2 <- praat("Get value at time...", input=FullPath(formantname), arguments=list(2, mid, "Hertz", "Linear"))
         
-        cat("f1: ", f1, "\n")
-        cat("f2: ", f2, "\n")
-        
-
-		
-		# Fix any undefined formants by checking right next to it, or assigning NA
-		#TODO: what will undefined in praat return to R?
-        ##It's currently printing out as --undefined--, so try the string of that value
+        # Fix any undefined formants by checking right next to it, or assigning NA
+        ##undefined in praat is currently printing out as --undefined--, so try comparing against the string of that value
 
 		if (f1 == "--undefined--"){
 			f1 <- praat("Get value at time...", input=FullPath(formantname), arguments=list(1, mid+10, "Hertz", "Linear"))
 			if (f1=="--undefined--"){
 				f1 <- "NA"
-            } else{ #TODO: R doesn't like trying to round the f1 string with "hertz" at the end
-				#truncate f1 to hundreths precision
-                #f1 <- round(f1, digits=2)
-			}
-		} else{
-            #f1 <- round(f1, digits=2)
+            }
 		}
 		
         
@@ -151,35 +137,35 @@ for(i in 1:numberofsegments){
 			f1 <- praat("Get value at time...", input=FullPath(formantname), arguments=list(2, mid+10, "Hertz", "Linear"))
 			if (f2=="--undefined--"){
 				f2 <- "NA"
-			} else{ #TODO: R doesn't like trying to round the f1 string with "hertz" at the end
-                #f2 <- round(f2, digits=2)
 			}
-		} else{
-            #f2 <- round(f2, digits=2)
 		}
         
-        
-        f1parse <- as.numeric(gsub("[^[:digit:].]", "\\1", f1))
-        f2parse <- as.numeric(gsub("[^[:digit:].]", "\\1", f2))
-        cat("f1parse: ", f1parse, "\n")
-        cat("f2parse: ", f2parse, "\n")
-        #f1 and f2 are returned with the Hertz tag, need to strip that from them
-		
+        #f1 and f2 are returned with Hertz tag as strings, need to strip them and make nums
+        #TODO: just make it f1 and f2 instead of f1parse and f2parse
+        #truncate f1 to hundreths precision
+        if (f1 != "--undefined--" || "NA"){
+            f1parse <- as.numeric(gsub("[^[:digit:].]", "\\1", f1))
+            f1parse <- round(f1, digits=2)
+        }
+        if (f2 != "--undefined--" || "NA"){
+            f2parse <- as.numeric(gsub("[^[:digit:].]", "\\1", f2))
+            f2parse <- round(f1, digits=2)
+        }
+
+        #no good way to comment out code in R so going to use if(FALSE)
         #if (FALSE){
             #begin if false
         
-		#TODO praat syntax
 		stparse <- round(stparse, digits=10)
 		etparse <- round(etparse, digits=10)
 		
+        
+        #TODO: Change line to row or column
 		if (guise == "spanish"){
-			#guise soundlabel st et precedingsegment followingsegment f1 f2
             line <- c(guise, soundlabel, stparse, etparse, precedingsegment, followingsegment, f1parse, f2parse)
 		} else if (guise == "spanish_plain"){
-            #guise soundlabel st et precedingsegment followingsegment f1 f2
             line <- c(guise, soundlabel, stparse, etparse, precedingsegment, followingsegment, f1parse, f2parse)
 		} else {
-            #guise soundlabel st et precedingsegment followingsegment f1 f2 wordlabel
             line <- c(guise, soundlabel, stparse, etparse, precedingsegment, followingsegment, f1parse, f2parse, wordlabel)
 		}
         
@@ -197,5 +183,4 @@ for(i in 1:numberofsegments){
 	}
 }
 
-#I don't think we need to remove the two objects, since this isn't a Praat workspace?
-#expand so it has pitch and duration, THEN rearrange php and R code that makes the graphs so that it'll just ask for the JSON from the database
+#TODO: expand so it has pitch and duration, THEN rearrange php and R code that makes the graphs so that it'll just ask for the JSON from the database
